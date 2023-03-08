cc349d7197d79180266083d3bb271e7eabaa4203
Saras Windecker <saras.windecker@gmail.com>
2023-03-08 19:03:57 -0400

    tidy ejercicios

--- Ejercicios.R
+++ Ejercicios.R
@@ -15,32 +15,3 @@
 # 2. Explora y elige una variable respuesta (continua) y una
 #     variables predictora (continua o categorica)
 # 3. Haz un modelo lineal y revise el resumen
-
-
-
-
-# tidyverse
-# shrinkage 
-group_mean <- mean(sleepstudy$Reaction)
-
-library(dplyr)
-by_subject <- sleepstudy |>
-  group_by(Subject) |>
-  summarise(mean_rt = mean(Reaction)) |>
-  mutate(mean_rt_centered = mean_rt - group_mean)
-
-
-## 1. elegir y abrir dataset
-# mtcars
-# iris
-# lme4::sleepstudy
-
-## 2. procesar en algun forma para practicar funciones
-
-## 3. pensar en una variable respuesta / predictores razonables
-
-## 4. evaluar si necesitas transformar predictores, 
-# confirmar si pueden usar modelo linear ?? 
-
-## 5. lm modelo
-
